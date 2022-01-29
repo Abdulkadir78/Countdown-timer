66bd521d23c9a472c73e27c30ba8d4189f19b49c
Abdulkadir78 <shekhaabdulkadir@gmail.com>
2022-01-30 00:51:14 +0530

    Init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Countdown timer
+
+A simple countdown timer.
+This is a solution to the [Launch countdown timer challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz-).
+<br />
+<br />
+
+### Screenshot
+
+![Countdown timer](./app-screenshots/countdown-timer.png)
---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+const defaultColors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./public/index.html", "./src/ts/**/*.ts"],
+  theme: {
+    fontFamily: {
+      sans: ["Red Hat Text", ...defaultTheme.fontFamily.sans],
+      serif: [...defaultTheme.fontFamily.serif],
+      mono: [...defaultTheme.fontFamily.mono],
+    },
+    colors: {
+      ...defaultColors,
+      primary: "hsl(345, 95%, 68%)",
+      secondary: "hsl(237, 18%, 59%)",
+      secondaryDark: "hsl(236, 21%, 26%)",
+      darkBlue: "hsl(235, 16%, 14%)",
+      veryDarkBlue: "hsl(234, 17%, 12%)",
+    },
+    container: {
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "10rem",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
